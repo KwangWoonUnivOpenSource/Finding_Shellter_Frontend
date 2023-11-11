3401c74315f48ea6bf0797e241bbe174800947fe
arthur <iii1003@naver.com>
2023-11-11 12:33:17 +0900

    수정

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import "./Footer.css"
+import style from "./Footer.module.css"
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
 export default function Footer(){
     return (
-        <div className="footer">
+        <div className={style.footer}>
             <Row>
                 <Col>프로젝트명</Col>
             </Row>
-            <div className="footerInfo">
+            <div className ={style.footerInfo}>
             <Row>
                 <Col>강지훈 조천산</Col>
-              
                 <Col>정 현 최윤호</Col>
             </Row>
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import './Home.css'
+import './Home.module.css'
 
 export default function Home(){
     return (
