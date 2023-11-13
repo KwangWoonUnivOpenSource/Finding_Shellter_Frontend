ed857c0e011417bd14d3ecb0aa5d773f2f3d919e
arthur <iii1003@naver.com>
2023-11-13 19:13:26 +0900

    11/13 arthur

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,25 +3,26 @@ import style from "./Header.module.css"
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Outlet } from 'react-router-dom';
  function Header(){
 
   return (
-    <>
+    <div>
+
       <Navbar  className = {style.nav} bg="primary" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">Finding Shelter</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#home">민방위 훈련 시 대피 요령</Nav.Link>
+            <Nav.Link href="#features">지진 발생 시 대피요령</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-    </>
+        <Outlet>
+          
+      </Outlet>
+    </div>
   );
 };
-    //    <div className = {style.header}>
-    //         <h2>Finding Shelter</h2>
-    //    </div>
 
 export default Header;
