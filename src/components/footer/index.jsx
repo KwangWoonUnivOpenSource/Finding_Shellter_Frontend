19cb2b6dab47bb3355fc3a2adb80df021d984f8f
arthur <iii1003@naver.com>
2023-11-20 20:51:02 +0900

    etc

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -14,7 +14,7 @@ export default function Footer(){
         <h3>오픈소스 소프트웨어 개발 8조</h3>
         <IoLogoGithub/>
         </div>
-        <button className = {style.btn}onClick={() => { navigate('/howtowar')}}>민방위 훈련 시 대피 요령</button>
+        <button className = {style.btn} onClick={() => { navigate('/howtowar')}}>민방위 훈련 시 대피 요령</button>
         <button className = {style.btn} onClick={() => { navigate('/howtogas');}}>화생방 발생 시 대피요령</button>
         <button className = {style.btn} onClick={() => { navigate('/howtoreport');}}>수상한 상황 발생 시 신고 요령</button>
         <button className = {style.btn} onClick={() => { navigate('/howtoprepare');}}>비상 시를 대피해 비치할 물자들</button>
